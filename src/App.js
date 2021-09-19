c378ffe2d6919e8b47a3f6eb97e67b3f8c60c0db
malakkhasawneh2 <malakkhasawneh2@gmail.com>
2021-09-19 11:51:00 +0300

    design page that allows adding panels

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { withAuth0 } from '@auth0/auth0-react';
+import LoginButton from './LoginButton';
+import Postpainting from './Postpainting';
+
+class App extends React.Component {
+
+  render() {
+    const{isAuthenticated}=this.props.auth0;
+    console.log('app', this.props);
+    return(
+      <>
+        <Router>
+            <Header />
+            <Switch>
+              <Route exact path="/">
+             {
+              isAuthenticated?<Postpainting/>:<LoginButton/>
+             }
+              </Route>              
+            </Switch>
+        </Router>
+      </>
+    );
+  }
 }
 
-export default App;
+export default withAuth0(App);
