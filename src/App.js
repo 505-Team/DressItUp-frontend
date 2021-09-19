60a110da8449de7b2ae722be356cd2c0c411d4db
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-09-19 11:46:55 +0300

    Home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './T-components/Header';
+// import IsLoadingAndError from './IsLoadingAndError';
+import Footer from './T-components/Footer';
+import Login from './T-components/Login';
+import Profile from './T-components/Profile';
+import Favorites from './T-components/Favorites';
+import { withAuth0 } from '@auth0/auth0-react';
+import Paint from './T-components/Paint';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+class App extends React.Component {
+
+  render() {
+    console.log('app', this.props);
+    const { isAuthenticated } = this.props.auth0;
+    return (
+      <>
+        <Router>
+          {/* <IsLoadingAndError> */}
+            <Header />
+            <Switch>
+              <Route exact path="/">
+                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+                {isAuthenticated ? <Favorites/>: <Login/>}
+              </Route>
+              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              <Route exact path="/Profile">
+
+                <Profile/>
+
+              </Route>
+            </Switch>
+            <Paint/>
+            <Footer />
+          {/* </IsLoadingAndError> */}
+        </Router>
+      </>
+    );
+  }
 }
 
-export default App;
+export default withAuth0(App);
