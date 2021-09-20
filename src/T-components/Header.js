5b57821a4f093146142c7f5664fcba77174db867
MusaabShalaldeh <musaabsh65@yahoo.com>
2021-09-20 15:48:56 +0300

    confirmed #3 merge is working fine

--- src/T-components/Header.js
+++ src/T-components/Header.js
@@ -1,52 +1,72 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import './Header.css';
-import LogoutButton from './LogoutButton';
+import "./Header.css";
+import LogoutButton from "./LogoutButton";
 // import LoginButton from './LoginButton';
-import { withAuth0 } from "@auth0/auth0-react"
-import LoginButton from './LoginButton';
-import artLogo from '../assets/artLogo/World Of Art.gif';
-import CartItem from './CartItem';
+import { withAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./LoginButton";
+import artLogo from "../assets/artLogo/World Of Art.gif";
+import CartItem from "./CartItem";
 
 class Header extends React.Component {
   render() {
     const { isAuthenticated } = this.props.auth0;
     return (
       <>
-                   <style>
-@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,700&display=swap');
-</style>
-        <Navbar className='navbarstaylheader' collapseOnSelect expand="lg" bg="black" variant="black">
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,700&display=swap');
+        </style>
+        <Navbar
+          className="navbarstaylheader"
+          collapseOnSelect
+          expand="lg"
+          bg="black"
+          variant="black"
+        >
+          <img className="logoDesign" src={artLogo} autoplay loop alt="logo" />
 
+          <Link to="/" id="link">
+            Home
+          </Link>
+          <Link to="/StorePage" id="link">
+            Gallery & Store
+          </Link>
+          {isAuthenticated && (
+            <Link to="/Favorites" id="link">
+              Favorites
+            </Link>
+          )}
+          {isAuthenticated && (
+            <Link to="/PostPainting" id="link">
+              Artwork
+            </Link>
+          )}
 
-          <img className='logoDesign' src={artLogo} autoplay loop
-          alt="logo" />
+          <Link to="/Aboutus" id="link">
+            About Us
+          </Link>
 
+          {isAuthenticated && (
+            <Link to="/CartItem" id="link">
+              Requested Items
+            </Link>
+          )}
+          {isAuthenticated && (
+            <Link to="/PostPainting" id="link">
+              Post Painting
+            </Link>
+          )}
+          {!isAuthenticated && <LoginButton style={{ color: "red" }} />}
 
-              <Link to="/" id="link">Home</Link>
-              <Link to="/StorePage" id="link">Gallery & Store</Link>
-              {isAuthenticated &&<Link to="/Favorites" id="link">Favorites</Link>}
-              {isAuthenticated && <Link to="/PostPainting" id="link">Artwork</Link>}
-             
-              <Link to="/Aboutus" id="link">About Us</Link>
-
-              {isAuthenticated && <Link to="/CartItem" id="link">Cart Item</Link>}
-              {isAuthenticated && <Link to="/PostPainting" id="link">Post Painting</Link>}
-              {!isAuthenticated &&  <LoginButton style={{color: 'red'}} />} 
-
-
-
-              {isAuthenticated && <LogoutButton />}
-              {/* { isAuthenticated ? <LogoutButton /> : <LoginButton /> } */}
-
-            </Navbar>
-
-
-          </>
-          );
+          {isAuthenticated && <LogoutButton />}
+          {/* { isAuthenticated ? <LogoutButton /> : <LoginButton /> } */}
+        </Navbar>
+      </>
+    );
   }
 }
 
-          export default withAuth0(Header);
+export default withAuth0(Header);
